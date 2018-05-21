f7cf96f3d78a8f3cf87c825cf70336de5aa89ada
barkeep8 <barkeep8@gmail.com>
2018-05-20 20:33:15 -0500

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,4 @@
+#REM   this is  a test.  This is only a test.  Do remarks still work like REM or only ##
+#   remember to do something interesting
+#   I like programming!   Don't you?
+#   you're not programming anything.   This is useless.
